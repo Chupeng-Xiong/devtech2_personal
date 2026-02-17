29be31a40efd9bc237b0646e173a0c94cac07639
cxiong82 <cxiong82@wisc.edu>
2026-02-16 21:39:38 -0600

    Fixed java file

--- DT2Part1.java
+++ DT2Part1.java
@@ -3,7 +3,7 @@ Class DevTech2Part1{
   public static void main(String args[]){
     try{
       connection dbCxn=DriverManager.getConnection(
-        "jbdc:mysql://localhost:53396/BoatsAndSailors","root","flappy_fish"  
+        "jbdc:mysql://localhost:53326/BoatsAndSailors","root","flappy_fish"  
       );
 
       String boatQuery = "select * from Boat";
@@ -12,12 +12,12 @@ Class DevTech2Part1{
       ResultsSet rsBoat=selectFromBoat.executeQuery(boatQuery);
       while(rsBoat.next()) {
         System.out.println(rsBoat.getInt(1)
-                + " " + rsBoat.getString(2)                  
-                + " " + rsBoat.getString(3)
-                + " " + rsBoat.getString(4)
+                + "  " + rsBoat.getString(2)                  
+                + "  " + rsBoat.getString(3)
+                + "  " + rsBoat.getFloat(4)
         );
       }
       dbCxn.close();
-    }catch(Exception e) {System.out.println(e);}
+    }catch(Exception e){ System.out.println(e);}
   }
 }
